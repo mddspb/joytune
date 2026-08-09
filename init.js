4949691b7ffe177c8617be892f11df977de4048d
mddspb <terrano.spb@gmail.com>
2026-08-09 19:35:23 -0300

    Rename JoyTune to JoyTuneRx in init.js

--- init.js
+++ init.js
@@ -1,9 +1,9 @@
 /**
- * JoyTune - OpenWebRX+ Gamepad Control Plugin Initialization Hook
+ * JoyTuneRx - OpenWebRX+ Gamepad Control Plugin Initialization Hook
  * Path: htdocs/plugins/receiver/joytune/init.js
  */
 
 (async () => {
-    await Plugins.load('joytune'); 
-    console.log("JoyTune - SDR Gamepad Control Bootloader: Success");
+    await Plugins.load('joytuneRx'); 
+    console.log("JoyTuneRx - SDR Gamepad Control Bootloader: Success");
 })();
